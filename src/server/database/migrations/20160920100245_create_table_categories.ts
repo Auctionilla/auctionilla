2c1f0e8041f80719fe8b0dcc7e2f8698d2ecb16c
cedrick buan <cedrickbuan@gmail.com>
2016-09-20 10:04:35 -0700

    added migration files

---
+++ src/server/database/migrations/20160920100245_create_table_categories.ts
@@ -0,0 +1,17 @@
+import { knex } from 'chen/sql';
+
+/**
+ * Apply database schema changes
+ * @param {knex} db
+ */
+export async function up(db: knex) {
+
+}
+
+/**
+ * Rollback database schema changes
+ * @param {knex} db
+ */
+export async function down(db: knex) {
+
+}
