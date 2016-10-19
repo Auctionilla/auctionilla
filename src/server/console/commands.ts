1963712f026ed70f0ea592d953721e37e41b273f
ceds <mac@Macs-MacBook-Pro.local>
2016-10-19 15:49:17 +0800

    command for getting emails and phonenumber fromm artnet.com

--- src/server/console/commands.ts
+++ src/server/console/commands.ts
@@ -1,3 +1,9 @@
 import { CommandMapping } from 'chen/console';
 
-export const COMMANDS: CommandMapping = {};
+export const COMMANDS: CommandMapping = {
+
+  'get-number-and-emails': {
+    class: 'getNumberAndEmails',
+    arguments: []
+  }
+};
